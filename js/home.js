74e8bda1c58be60c113fdf884ebfdc1a48b96bb6
Claire <40320476+justaskclaire@users.noreply.github.com>
2025-08-10 23:31:18 -0500

    Add typing animation to home page

---
+++ js/home.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+  new Typed('#greeting', {
+    strings: ["Hi, I'm Claire.<br>FL > VA > CT > CA > ID > CT > NM > ID > NM > OK > TX"],
+    typeSpeed: 50
+  });
+});
